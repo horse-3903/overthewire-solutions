b65a292a38a86371f790257fe16b45528814ca44
horse-3903 <chongchoonhourafael@gmail.com>
2024-04-15 14:26:37 +0800

    add : bandit3

---
+++ bandit/code/bandit3.py
@@ -0,0 +1,52 @@
+import pwn
+
+pwn.context.log_level = "WARNING"
+
+n = 3
+username = f"bandit{n}"
+hostname = "bandit.labs.overthewire.org"
+password = None
+
+with open(f"./bandit/password/{username}-password.txt") as f:
+    password = f.read()
+
+print(f"Connecting to {hostname}:2220")
+print(f"Username : {username}")
+print(f"Password : {password}")
+
+connect = pwn.ssh(host=hostname, user=username, password=password, port=2220)
+
+def send_command(command: str) -> pwn.tubes.ssh.ssh_channel:
+    print(f"Sending process : `{command}`", end="...")
+    channel = connect.system(command)
+    print("Done")
+
+    return channel
+
+def receive_output(channel: pwn.tubes.ssh.ssh_channel) -> bytes:
+    print("Receiving output", end="...")
+    output = channel.recvline(keepends=False)
+    print("Done")
+
+    print(output.decode())
+
+    return output
+
+if connect.connected():
+    # find all files in current directory
+    channel = send_command("ls")
+    dir = receive_output(channel).decode()
+
+    # find all files, including hidden files
+    channel = send_command(f"ls -a {dir}")
+    result = receive_output(channel).decode()
+    result = result.split()[2:]
+
+    # find all files, including hidden files
+    channel = send_command(f"cat {dir}/{result[0]}")
+    result = receive_output(channel).decode()
+
+    connect.close()
+
+with open(f"./bandit/password/{username[:-1]}{int(username[-1])+1}-password.txt", "w+") as f:
+    f.write(result)

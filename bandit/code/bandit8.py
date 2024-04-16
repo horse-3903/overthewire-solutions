d633bd36d33520bf10a3d24455151294c44bc0ad
horse-3903 <chongchoonhourafael@gmail.com>
2024-04-16 19:55:08 +0800

    add : bandit8

---
+++ bandit/code/bandit8.py
@@ -0,0 +1,58 @@
+import os
+import pwn
+
+pwn.context.log_level = "WARNING"
+
+n = os.path.basename(__file__)
+n = n[n.find("bandit")+6]
+
+username = f"bandit{n}"
+hostname = "bandit.labs.overthewire.org"
+password = None
+
+with open(f"./bandit/password/{username}-password.txt") as f:
+    password = f.read()
+
+print(f"Connecting to {hostname}:2220")
+print(f"Username : {username}")
+print(f"Password : {password}")
+print()
+
+connect = pwn.ssh(host=hostname, user=username, password=password, port=2220)
+
+def send_command(command: list[str]) -> pwn.tubes.ssh.ssh_channel:
+    command = " ".join(command)
+
+    print(f"Sending process : `{command}`", end="...")
+    channel = connect.system(command)
+    print("Done")
+    
+    print()
+
+    return channel
+
+def receive_output(channel: pwn.tubes.ssh.ssh_channel) -> str:
+    print("Receiving output", end="...")
+    output = channel.recv()
+    output = output.decode()
+
+    output = output.strip()
+    output = output.strip("\n")
+    print("Done")
+
+    print(output)
+    print()
+
+    return output
+
+if connect.connected():
+    channel = send_command(["ls"])
+    result = receive_output(channel)
+
+    channel = send_command(["sort", result, "|", "uniq", "-u"])
+    result = receive_output(channel)
+
+    connect.close()
+
+with open(f"./bandit/password/{username[:-1]}{int(username[-1])+1}-password.txt", "w+") as f:
+    f.write(result)

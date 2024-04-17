2bfe276de376f9d74baa647fca1cd38393f2e56a
horse-3903 <chongchoonhourafael@gmail.com>
2024-04-17 13:15:53 +0800

    add : bandit00 - bandit09

---
+++ bandit/code/bandit04.py
@@ -0,0 +1,71 @@
+import os
+import pwn
+
+pwn.context.log_level = "WARNING"
+
+n = os.path.basename(__file__)
+n = n.split(".")[0]
+n = n[n.find("bandit")+6:]
+
+username = f"bandit{n}"
+hostname = "bandit.labs.overthewire.org"
+password = None
+
+with open(f"./bandit/password/{username}-password.txt") as f:
+    password = f.read()
+
+print(f"Connecting to {hostname}:2220")
+print(f"Username : {username}")
+print(f"Password : {password}")
+print()
+
+connect = pwn.ssh(host=hostname, user=username, password=password, port=2220)
+
+def send_command(command: list[str], connect: pwn.ssh = connect) -> pwn.tubes.ssh.ssh_channel:
+    command = " ".join(command)
+    
+    print(f"Sending process : `{command}`", end="...")
+    channel = connect.system(command)
+    print("Done")
+    
+    print()
+
+    return channel
+
+def receive_output(channel: pwn.tubes.ssh.ssh_channel) -> str:
+    print("Receiving output", end="...")
+    output = channel.recv()
+    output = output.decode()
+
+    output = output.strip()
+    output = output.strip("\n")
+    print("Done")
+
+    print(output)
+    print()
+
+    return output
+
+if connect.connected():
+    # find all files in current directory
+    channel = send_command(["ls"])
+    result = receive_output(channel)
+
+    channel = send_command(["ls", result])
+    files = receive_output(channel)
+    files = [f"{result}/{f}" for f in files.split()]
+
+    for f in files:
+        channel = send_command(["file", f])
+        result = receive_output(channel)
+
+        if "ASCII text" in result:
+            break
+
+    channel = send_command(["cat", f])
+    result = receive_output(channel)
+
+    connect.close()
+
+with open(f"./bandit/password/bandit{int(n)+1}-password.txt", "w+") as f:
+    f.write(result)
---
+++ bandit/code/bandit09.py
@@ -0,0 +1,64 @@
+import os
+import pwn
+
+pwn.context.log_level = "WARNING"
+
+n = os.path.basename(__file__)
+n = n.split(".")[0]
+n = n[n.find("bandit")+6:]
+
+username = f"bandit{n}"
+hostname = "bandit.labs.overthewire.org"
+password = None
+
+with open(f"./bandit/password/{username}-password.txt") as f:
+    password = f.read()
+
+print(f"Connecting to {hostname}:2220")
+print(f"Username : {username}")
+print(f"Password : {password}")
+print()
+
+connect = pwn.ssh(host=hostname, user=username, password=password, port=2220)
+
+def send_command(command: list[str], connect: pwn.ssh = connect) -> pwn.tubes.ssh.ssh_channel:
+    command = " ".join(command)
+
+    print(f"Sending process : `{command}`", end="...")
+    channel = connect.system(command)
+    print("Done")
+    
+    print()
+
+    return channel
+
+def receive_output(channel: pwn.tubes.ssh.ssh_channel) -> str:
+    print("Receiving output", end="...")
+    output = channel.recv()
+    output = output.decode()
+
+    output = output.strip()
+    output = output.strip("\n")
+    print("Done")
+
+    print(output)
+    print()
+
+    return output
+
+if connect.connected():
+    channel = send_command(["ls"])
+    result = receive_output(channel)
+
+    channel = send_command(["cat", result, "|", "strings", "|", "grep", '"^="'])
+    options = receive_output(channel)
+    options = [s.replace("=", "") for s in options.splitlines()]
+    options = [s.strip() for s in options]
+    options = sorted(options, key=len)
+    
+    result = options[-1]
+
+    connect.close()
+
+with open(f"./bandit/password/bandit{int(n)+1}-password.txt", "w+") as f:
+    f.write(result)

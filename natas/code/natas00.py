fd426be6613f1cddd46262439fbe4cad85091ead
horse-3903 <chongchoonhourafael@gmail.com>
2024-04-23 08:50:11 +0800

    add : natas00

---
+++ natas/code/natas00.py
@@ -0,0 +1,55 @@
+import os
+import requests
+
+from bs4 import BeautifulSoup, Tag
+from bs4 import Comment
+
+file = os.path.basename(__file__).split(".")
+file = file[0]
+file = file[file.find("natas")+5:]
+
+level = int(file)
+
+username = f"natas{level}"
+password = "natas0"
+website = f"http://{username}.natas.labs.overthewire.org"
+
+# with open(f"./natas/password/natas{level:02}-password.txt", "r") as f:
+#     password = f.read()
+
+def natas_connect(website: str, username: str, password: str, params: dict = None) -> Tag:
+    print(f"Connecting to {website}")
+    print(f"Username : {username}")
+    print(f"Password : {password}")
+    print()
+
+    r = requests.get(website, auth=(username, password), params=params)
+
+    print("Parsing HTML", end="...")
+    soup = BeautifulSoup(r.content, "html.parser")
+    print("Done\n")
+
+    print("Retrieving <body> tag", end="...")
+    body = soup.find("body")
+    print("Done\n")
+
+    print(body)
+    print()
+
+    return body
+
+body = natas_connect(website=website, username=username, password=password)
+
+print("Finding comments", end="...")
+comments = body.find_all(string=lambda element: isinstance(element, Comment))
+print("Done\n")
+
+result = comments[0]
+result = result.split()
+
+result = result[-1]
+
+print(f"Password found : {result}")
+
+with open(f"./natas/password/natas{level+1:02}-password.txt", "w+") as f:
+    f.write(result)

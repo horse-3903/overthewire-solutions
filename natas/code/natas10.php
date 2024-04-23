375b645c7e3d6ccf0ef4a490a1d2a34fa51128dc
horse-3903 <chongchoonhourafael@gmail.com>
2024-04-24 07:24:23 +0800

    add : natas10

---
+++ natas/code/natas10.php
@@ -0,0 +1,14 @@
+<?php
+$key = "";
+
+if (array_key_exists("needle", $_REQUEST)) {
+    $key = $_REQUEST["needle"];
+}
+
+if ($key != "") {
+    if (preg_match('/[;|&]/', $key)) {
+        print "Input contains an illegal character!";
+    } else {
+        passthru("grep -i $key dictionary.txt");
+    }
+}
